39b6d74e9376266f4a7da2da5d8e0c56c91e7f82
travis <travisdavidnelson@gmail.com>
2019-06-01 02:06:14 -0600

    add Timeline author

--- src/main/java/com/tdn/timeline/Timeline.java
+++ src/main/java/com/tdn/timeline/Timeline.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Timeline extends TimelineEvent {
 	private String id = null;
+	private String author = "Travis David Nelson";
 	private TimelineConfig config = null;
 	
 	private List<TimelineEvent> backgroundEvents;
@@ -26,6 +27,13 @@ public class Timeline extends TimelineEvent {
 		this.id = id;
 	}
 	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
 	public TimelineConfig getConfig() {
 		return config;
 	}
